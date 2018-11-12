bd028735b3cbd3b6c0688d518a260b4472b08a02
Mike Morrone <Hanegwen725@gmail.com>
2018-11-12 14:38:01 -0600

    Testing Trees

--- ChatRoom/ChatRoom/Hub/Chat.cs
+++ ChatRoom/ChatRoom/Hub/Chat.cs
@@ -7,9 +7,29 @@ namespace Microsoft.Azure.SignalR.Samples.ChatRoom
 {
     public class Chat : Hub
     {
+        
+        DataTree tree = new DataTree();
         public void BroadcastMessage(string name, string message)
         {
-            Clients.All.SendAsync("broadcastMessage", name, message);
+             Clients.All.SendAsync("broadcastMessage", name, message);
+
+            switch (message)
+            {
+                case "Add Node:":
+                    tree.AddNode(message + i, null);
+                    BroadcastMessage("Server", tree.branches.Count.ToString());
+                    foreach (TreeNode branch in tree.branches)
+                    {
+                        BroadcastMessage("Server", branch.Content);
+                    }
+                    
+                    BroadcastMessage("Server", i++.ToString());
+                    i++;
+                    break;
+                default:
+                    break;
+            }
+
         }
 
         public void Echo(string name, string message)
---
+++ ChatRoom/ChatRoom/TreeFolder/INode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.SignalR.Samples.ChatRoom
+{
+    interface INode
+    {
+        
+        string ID { get; set; }
+
+        string Content { get; set; }
+
+        bool IsReady { get; set; }
+        INode Parent { get; set; }
+        List<TreeNode> Children { get; set; }
+
+        
+    }
+}
