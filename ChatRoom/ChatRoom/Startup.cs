66fe3b1d1cd4f0f59839286da9b475fed1d5ec8d
Mike Morrone <Hanegwen725@gmail.com>
2018-11-12 12:52:30 -0600

    AddChatRoom

---
+++ ChatRoom/ChatRoom/Startup.cs
@@ -0,0 +1,36 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Azure.SignalR.Samples.ChatRoom
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSignalR()
+                    .AddAzureSignalR("Endpoint=https://chatapp2018.service.signalr.net;AccessKey=+Qzv7c170xw8JEGNZ8tN1h72oHFDUWyIrG1f86QsLYQ=;Version=1.0;");
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+            app.UseFileServer();
+            app.UseAzureSignalR(routes =>
+            {
+                routes.MapHub<Chat>("/chat");
+            });
+        }
+    }
+}
