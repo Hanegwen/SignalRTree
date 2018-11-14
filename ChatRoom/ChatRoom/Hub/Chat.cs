b7ced155bdb9cd6e2b93e906bbbaa2d40b8c77e2
Mike Morrone <Hanegwen725@gmail.com>
2018-11-14 08:42:59 -0600

    Refactor is Done

--- ChatRoom/ChatRoom/Hub/Chat.cs
+++ ChatRoom/ChatRoom/Hub/Chat.cs
@@ -10,6 +10,23 @@ namespace Microsoft.Azure.SignalR.Samples.ChatRoom
         public void BroadcastMessage(string name, string message)
         {
             Clients.All.SendAsync("broadcastMessage", name, message);
+
+            switch (message)
+            {
+                case "Add Node:":
+                    tree.AddNode(message + i, null);
+                    BroadcastMessage("Server", tree.branches.Count.ToString());
+                    foreach (TreeNode branch in tree.branches)
+                    {
+                        BroadcastMessage("Server", branch.Content);
+                    }
+
+                    BroadcastMessage("Server", i++.ToString());
+                    i++;
+                    break;
+                default:
+                    break;
+            }
         }
 
         public void Echo(string name, string message)
---
+++ ChatRoom/ChatRoom/TreeFolder/TreeNode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Azure.SignalR.Samples.ChatRoom
+{
+    class TreeNode : INode
+    {
+        string id = " ";
+        public string ID { get => id; set => id = value; }
+
+        string content = " ";
+        public string Content { get => content; set => content = value; }
+
+        bool isReady = false;
+        public bool IsReady { get => isReady; set => isReady = value; }
+
+        INode parent = null;
+        public INode Parent { get => parent; set => parent = value; }
+
+
+        List<TreeNode> INode.Children { get => children; set => children = value; }
+
+        List<TreeNode> children = new List<TreeNode>();
+ 
+
+        public TreeNode(string ID, string Content, bool IsReady, INode Parent)
+        {
+            this.ID = ID;
+            this.Content = Content;
+            this.IsReady = IsReady;
+            this.Parent = Parent;
+
+            if (Parent != null)
+            {
+                Parent.Children.Add(this);
+            }
+        }
+    }
+}
